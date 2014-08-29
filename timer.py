856dd163e6e9f1488050b9ddc950290f00bf2815
Jonathan E. T. Means <jetmeans@gmail.com>
2014-08-29 09:22:22 -0700

    Add comments; move playlist creation from play_tune() to __init__()

--- timer.py
+++ timer.py
@@ -5,6 +5,7 @@ import pygame
 
 class App():
     def __init__(self):
+        #Set up window
         self.root = tk.Tk()
         self.label = tk.Label(text="", font=("Helvetica", 150))
         self.label.pack()
@@ -13,13 +14,20 @@ class App():
         self.pomo_count = 0
         self.is_break = False
 
+        #Set up playlist
+        self.tunes = ['sound.wav']
+        for i in range(1, 6):
+            self.tunes.append("sound%d.wav" % i)
+
+
         self.reset_timer()
         self.root.mainloop()
 
     def reset_timer(self):
 
+        #Set timer for break
+        #A longer, 10-minute break occurs every 4 pomodoros
         if self.is_break:
-
             if self.pomo_count is 4:
                 difference = datetime.timedelta(seconds=60*10)
                 self.pomo_count = 0
@@ -28,6 +36,9 @@ class App():
 
             self.is_break = False
 
+        #Set 25-minute pomo-timer
+        #If this were a serious project, there'd be fewer
+        #magic number
         else:
             self.pomo_count += 1
             difference = datetime.timedelta(seconds=60*25)
@@ -41,17 +52,16 @@ class App():
         m, s = elapsed.seconds/60, elapsed.seconds%60
         self.label.configure(text="%02d:%02d" % (m, s))
 
+        #Timer has hit the bottom
         if m <=0 and s <= 0:
             self.reset_timer()
             self.play_tune()
 
+        #Wait for 1 second to pass and run again
         self.root.after(1000, self.update_clock)
 
     def play_tune(self):
-        tunes = ['sound.wav']
-        for i in range(1, 6):
-            tunes.append("sound%d.wav" % i)
-        soundfile = random.choice(tunes)
+        soundfile = random.choice(self.tunes)
         pygame.init()
         pygame.mixer.Sound(soundfile).play()
 
